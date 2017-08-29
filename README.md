3759e56506b72617eb5593ab5ee12982b095908f
Haitao <31330123+taozer@users.noreply.github.com>
2017-08-29 12:17:13 +0800

    Update and rename README.md to test01.cc
    add cout hello,github

--- README.md
+++
@@ -1,2 +0,0 @@
-# TEST
-This is my first github project!
---
+++ test01.cc
@@ -0,0 +1,11 @@
+# TEST
+This is my first github project!
+
+#include <iostream>
+using namespace std;
+  
+int main ()
+{
+  cout << 'This is my first github project!' << endl;
+  return 0;
+}
